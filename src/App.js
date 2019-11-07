d10bd51a3122a5fdc4f2a78d38c5395f046e0c0a
Peter Walker <25408515+peteawalk@users.noreply.github.com>
2019-11-07 16:45:35 -0600

    cars now displaying

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import CarCard from "./components/CarCard";
+import Wrapper from "./components/Wrapper";
+import Jumbotron from "./components/Jumbotron";
+import cars from "./cars.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // Setting this.state.cars to the cars json array
+  state = {
+    cars
+  };
+
+  removeFriend = id => {
+    // Filter this.state.cars for cars with an id not equal to the id being removed
+    const cars = this.state.cars.filter(car => car.id !== id);
+    // Set this.state.cars equal to the new cars array
+    this.setState({ cars });
+  };
+
+  // Map over this.state.cars and render a FriendCard component for each friend object
+  render() {
+    return (
+      <Wrapper>
+        <Jumbotron />
+        {this.state.cars.map(car => (
+          <CarCard
+            id={car.id}
+            image={car.image}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Jumbotron/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+// Navbar Score Board
+function Jumbotron(props) {
+  return (
+    <div className="container">
+      <div className="jumbotron">
+        <h1 class="display-3">Mustang Memory</h1>
+        <hr class="my-2" />
+        <p class="lead">Choose one car only. Any repeats and you lose!</p>
+      </div>
+    </div>
+  );
+}
+
+export default Jumbotron;
